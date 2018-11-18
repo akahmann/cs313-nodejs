1802bfdf14f99ccded5619252f583752b0812ada
akahmann <akahmann93@gmail.com>
2018-11-17 17:24:42 -0700

    asn9

--- asn09/index.js
+++ asn09/index.js
@@ -5,7 +5,7 @@ var calc = require('./calc.js');
 var app = express();
 
 app.set('port', process.env.PORT || 5000)
-  .use(express.static(__dirname + '/asn09'))
+  .use(express.static(__dirname + '/public'))
   .set('views', __dirname + '/views')
   .set('view engine', 'ejs')
   .get('/calc', calc.playGame) //TODO: update playGame to function name
