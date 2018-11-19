9173637ee4a237fbac988c7fc2db2e17f33afc38
akahmann <akahmann93@gmail.com>
2018-11-19 16:46:27 -0700

    asn09

--- asn09/calc.js
+++ asn09/calc.js
@@ -1,48 +1,70 @@
-function playGame(req, res) {
-   var url_parts = url.parse(req.url, true);
-   var query = url_parts.query;
-   var weight = +query.weight;
-   var typeOfMail = query.typeOfMail;
+function getCpuChoice() {
+   // Use a random object to make a selection for the cpu
+   // then return it
 
-   if(typeOfMail == "Large Envelopes(Flats)") {
-      var price = 1.00;
-      for(var i = 0; i < weight - 1; i++) {
-         price += 0.21;
-      }
-   }
-
-   if(typeOfMail == "Letters(stamped)") {
-      var price = 0.5;
-      for(var i = 0; i < weight - 1; i++) {
-         price += 0.21;
-      }
-      if(weight > 3.5) {
-         price = 1.13;
-      }
-   }
-
-   if(typeOfMail == "Letters(metered)") {
-      var price = 0.47;
-      for(var i = 0; i < weight - 1; i++) {
-            price += 0.21;
-         }
-      if(weight > 3.5) {
-         price = 1.10;
-      }
-   }
-
-   if(typeOfMail == "First-Class Package Service--Retail") {
-      var price = 3.5;
-      if(weight > 4) {
-         price = 3.75;
-      }
-      if(weight > 8) {
-         price = 4.10;
-         for(var i = 8; i < weight - 1; i++) {
-            price += .35;
-         }
-      }
-   }
-
-   res.render("formResponse", {price:price,typeOfMail:typeOfMail,weight:weight});
+   return 'rock';
 }
+
+function playGame(req, res) {
+   console.log('Playing the game from a different file...');
+
+   var player = req.query.player_choice;
+   console.log('The player choice is: ' + player);
+
+   var cpu = getCpuChoice()
+
+   var stuff = {player: player, cpu: cpu};
+
+   res.render('results', stuff);
+}
+
+module.exports = {playGame: playGame};
+
+// function calculate(req, res) {
+//    var url_parts = url.parse(req.url, true);
+//    var query = url_parts.query;
+//    var weight = +query.weight;
+//    var typeOfMail = query.typeOfMail;
+
+//    if(typeOfMail == "Large Envelopes(Flats)") {
+//       var price = 1.00;
+//       for(var i = 0; i < weight - 1; i++) {
+//          price += 0.21;
+//       }
+//    }
+
+//    if(typeOfMail == "Letters(stamped)") {
+//       var price = 0.5;
+//       for(var i = 0; i < weight - 1; i++) {
+//          price += 0.21;
+//       }
+//       if(weight > 3.5) {
+//          price = 1.13;
+//       }
+//    }
+
+//    if(typeOfMail == "Letters(metered)") {
+//       var price = 0.47;
+//       for(var i = 0; i < weight - 1; i++) {
+//             price += 0.21;
+//          }
+//       if(weight > 3.5) {
+//          price = 1.10;
+//       }
+//    }
+
+//    if(typeOfMail == "First-Class Package Service--Retail") {
+//       var price = 3.5;
+//       if(weight > 4) {
+//          price = 3.75;
+//       }
+//       if(weight > 8) {
+//          price = 4.10;
+//          for(var i = 8; i < weight - 1; i++) {
+//             price += .35;
+//          }
+//       }
+//    }
+
+//    res.render("formResponse", {price:price,typeOfMail:typeOfMail,weight:weight});
+// }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-var calc = require('./asn09/calc.js');
+//var calc = require('/asn09/calc.js');
+const path = require('path');
+var url = require('url');
 
 var app = express();
 
@@ -8,7 +10,52 @@ app.set('port', process.env.PORT || 5000)
   .use(express.static(__dirname + '/asn09'))
   .set('views', __dirname + '/asn09/views')
   .set('view engine', 'ejs')
-  .get('/results', calc.playGame) //TODO: update playGame to function name
+  .get("/results", function(req,res)
+   {
+      var url_parts = url.parse(req.url, true);
+      var query = url_parts.query;
+      var weight = +query.weight;
+      var letters = query.letters;
+
+      if(letters == "Letters Envelopes (Flats)") {
+         var price = 1.00;
+         for(var i = 0; i < weight - 1; i++) {
+            price += 0.21;
+         }
+      }
+      if(letters == "Letters (Stamped)") {
+         var price = 0.5;
+         for(var i = 0; i < weight - 1; i++) {
+            price += 0.21;
+         }
+         if(weight > 3.5) {
+            price = 1.13;
+         }
+      }
+      if(letters == "Letters (Metered)") {
+         var price = 0.47;
+         for(var i = 0; i < weight - 1; i++) {
+            price += 0.21;
+         }
+         if(weight > 3.5) {
+            price = 1.10;
+         }
+      }
+      if(letters == "First-Class Package Service - Retail") {
+         var price = 3.5;
+         if(weight > 4) {
+            price = 3.75;
+         }
+         if(weight > 8) {
+            price = 4.10;
+            for(var i = 8; i < weight - 1; i++) {
+               price += .35;
+            }
+         }
+      }
+      res.render("results", {price:price, letters:letters, weight:weight});
+   })
+  //.get('/results', calc.calculate) //fix this later
   .listen(app.get('port'), function() {
    console.log('Listening on port: ' + app.get('port'));
   });
